5d6ca7b21aec634cfa06f4ba40ffb6823c6d31f6
robot-nano <adaofu01@gmail.com>
2021-01-15 21:12:31 +0800

    add object.h macro define & ObjectRef

--- include/cvt/runtime/container.h
+++ include/cvt/runtime/container.h
@@ -18,7 +18,25 @@ class StringObj : public Object {
 
   static constexpr const uint32_t _type_index = TypeIndex::kRuntimeString;
   static constexpr const char* _type_key = "runtime.String";
+  CVT_DECLARE_FINAL_OBJECT_INFO(StringObj, Object);
 
+ private:
+  class FromStd;
+
+  friend class String;
+};
+
+
+
+class StringObj::FromStd : public StringObj {
+ public:
+  explicit FromStd(std::string other) : data_container(std::move(other)) {}
+
+ private:
+  /*! \brief Container that holds the memory. */
+  std::string data_container;
+
+  friend class String;
 };
 
 }  // namespace runtime
--- include/cvt/runtime/object.h
+++ include/cvt/runtime/object.h
@@ -250,8 +250,171 @@ class ObjectPtr {
   friend ObjectPtr<BaseType> GetObjectPtr(ObjType* ptr);
 };
 
-#define CVT_DECLARE_BASE_OBJECT_INFO(TypeName, ParentType) \
-  
+/*! \brief Base class of all object reference */
+class ObjectRef {
+ public:
+  ObjectRef() = default;
+  explicit ObjectRef(ObjectPtr<Object> data) : data_(data) {}
+
+  bool same_as(const ObjectRef& other) const { return data_ == other.data_; }
+
+  bool operator==(const ObjectRef& other) const { return data_ == other.data_; }
+
+  bool operator!=(const ObjectRef& other) const { return data_ != other.data_; }
+
+  bool operator<(const ObjectRef& other) const { return data_.get() < other.data_.get(); }
+
+  bool defined() const { return data_ != nullptr; }
+
+  const Object* get() const { return data_.get(); }
+
+  const Object* operator->() const { return get(); }
+
+  bool unique() const { return data_.unique(); }
+
+  int use_count() const { return data_.use_count(); }
+
+  template <typename ObjectType>
+  inline const ObjectType* as() const;
+
+  using ConatinerType = Object;
+
+  static constexpr bool _type_is_nullable = true;
+
+ protected:
+  ObjectPtr<Object> data_;
+
+  Object* get_mutable() const { return data_.get(); }
+
+  template <typename T>
+  static T DowncastNoCheck(ObjectRef ref) {
+    return T(std::move(ref.data_));
+  }
+
+  static void FFIClearAfterMove(ObjectRef* ref) { ref->data_.data_ = nullptr;  }
+
+  template <typename ObjectType>
+  static ObjectPtr<ObjectType> GetDataPtr(const ObjectRef& ref) {
+    return ObjectPtr<ObjectType>(ref.data_.data_);
+  }
+  // friend classes.
+  friend struct ObjectPtrHash;
+  friend class CVTRetValue;
+  friend class CVTArgsSetter;
+  friend class ObjectInternal;
+  template <typename SubRef, typename BaseRef>
+  friend SubRef Downcast(BaseRef ref);
+};
+
+template <typename BaseType, typename ObjectType>
+inline ObjectPtr<BaseType> GetObjectPtr(ObjectType* ptr);
+
+struct ObjectPtrHash {
+
+};
+
+/*!
+ * \brief helper macro to declare a base object type that can be inherited.
+ * \param TypeName The name of the current type.
+ * \param ParentType The name of the ParentType
+ */
+#define CVT_DECLARE_BASE_OBJECT_INFO(TypeName, ParentType)                                      \
+  static_assert(!ParentType::_type_final, "ParentObj marked as final");                         \
+  static uint32_t RuntimeTypeIndex() {                                                          \
+    static_assert(TypeName::_type_child_slots == 0 || ParentType::_type_child_slots == 0 ||     \
+                      TypeName::_type_child_slots < ParentType::_type_child_slots_can_overflow, \
+                  "Need to set _type_child_slots when parent specifices it.");                  \
+    if (TypeName::_type_index != ::cvt::runtime::TypeIndex::kDynamic) {                         \
+      return TypeName::_type_index;                                                             \
+    }                                                                                           \
+    return _GetOrAllocRuntimeTypeIndex();                                                       \
+  }                                                                                             \
+  static uint32_t _GetOrAllocRuntimeTypeIndex() {                                               \
+    static uint32_t tidx = Object::GetOrAllocRuntimeTypeIndex(                                  \
+        TypeName::_type_key, TypeName::_type_index, ParentType::_GetOrAllocRuntimeTypeIndex(),  \
+        TypeName::_type_child_slots, TypeName::_type_child_slots_can_overflow);                 \
+    return tidx;                                                                                \
+  }
+
+/*!
+ * \brief helper macro to declare type information in a final class.
+ * \param TypeName The name of the current type.
+ * \param ParentType The name of the ParentType.
+ */
+#define CVT_DECLARE_FINAL_OBJECT_INFO(TypeName, ParentType) \
+  static const constexpr bool _type_final = true;           \
+  static const constexpr int _type_child_slots = 0;         \
+  CVT_DECLARE_BASE_OBJECT_INFO(TypeName, ParentType)
+
+/*! \brief helper macro to suppress unused warning */
+#if defined(__GNUC__)
+#define CVT_ATTRIBUTE_UNUSED __attribute__((unused))
+#else
+#define CVT_ATTRIBUTE_UNUSED
+#endif
+
+#define CVT_STR_CONCAT_(__x, __y) __x##__y
+#define CVT_STR_CONCAT(__x, __y) CVT_STR_CONCAT_(__x, __y)
+
+#define CVT_OBJECT_REG_VAR_DEF static CVT_ATTRIBUTE_UNUSED uint32_t __make_object_tid
+
+/*!
+ * \brief Helper macro to register the object type to runtime.
+ * Makes sure that the runtime type table is correctly populated.
+ *
+ * Use this macro in the cc file for each terminal class.
+ */
+#define CVT_REGISTER_OBJECT_TYPE(TypeName) \
+  CVT_STR_CONCAT(CVT_OBJECT_REG_VAR_DEF, __COUNTER__) = TypeName::_GetOrAllocRuntimeTypeIndex()
+
+/*!
+ * \brief Define the default copy/move constructor and assign operator
+ * \param TypeName The class typename.
+ * */
+#define CVT_DEFINE_DEFAULT_COPY_MOVE_AND_ASSIGN(TypeName) \
+  TypeName(const TypeName& other) = default;              \
+  TypeName(TypeName&& other) = default;                   \
+  TypeName& operator=(const TypeName& other) = default;   \
+  TypeName& operator=(TypeName&& other) = default;
+
+/*!
+ * \brief Define object reference methods.
+ * \param TypeName The object type name
+ * \param
+ */
+#define CVT_DEFINE_OBJECT_REF_METHODS(TypeName, ParentType, ObjectName)                        \
+  TypeName() = default;                                                                        \
+  explicit TypeName(::cvt::runtime::ObjectPtr<::cvt::runtime::Object> n) : ParentType(n) {}    \
+  CVT_DEFINE_DEFAULT_COPY_MOVE_AND_ASSIGN(TypeName);                                           \
+  const ObjectName* operator->() const { return static_cast<const ObjectName*>(data_.get()); } \
+  const ObjectName* get() const { return operator->(); }                                       \
+  using ContainerType = ObjectName;
+
+#define CVT_DEFINE_NOTNULLABLE_OBJECT_REF_METHODS(TypeName, ParentType, ObjectName)            \
+  explicit TypeName(::cvt::runtime::ObjectPtr<::cvt::runtime::Object> n) : ParentType(n) {}    \
+  CVT_DEFINE_DEFAULT_COPY_MOVE_AND_ASSIGN(TypeName);                                           \
+  const ObjectName* operator->() const { return static_cast<const ObjectName*>(data_.get()); } \
+  const ObjectName* get() const { return operator->(); }                                       \
+  static constexpr bool _type_is_nullable = false;                                             \
+  using ContainerType = ObjectName;
+
+#define CVT_DEFINE_MUTABLE_OBJECT_REF_METHODS(TypeName, ParentType, ObjectName)             \
+  TypeName() = default;                                                                     \
+  CVT_DEFINE_DEFAULT_COPY_MOVE_AND_ASSIGN(TypeName);                                        \
+  explicit TypeName(::cvt::runtime::ObjectPtr<::cvt::runtime::Object> n) : ParentType(n) {} \
+  ObjectName* operator->() const { return static_cast<ObjectName*>(data_.get()); }          \
+  using ContainerType = ObjectName;
+
+#define CVT_DEFINE_OBJECT_REF_COW_METHOD(ObjectName)     \
+  ObjectName* CopyOnWrite() {                            \
+    ICHECK(data_ != nullptr);                            \
+    if (!data_.unique()) {                               \
+      auto n = make_object<ObjectName>(*(operator->())); \
+      ObjectPtr<Object>(std::move(n)).swap(data_);       \
+    }                                                    \
+    return static_cast<ObjectName*>(data_.get());        \
+  }
+
 #if CVT_OBJECT_ATOMIC_REF_COUNTER
 
 inline void Object::IncRef() { ref_counter_.fetch_add(1, std::memory_order_relaxed); }
